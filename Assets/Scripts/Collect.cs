54e58a0e48879a56ffd4dd12ecd3e05ae9d1233d
Tyler Hendrickson <THendriXon@users.noreply.github.com>
2017-10-02 13:31:39 -0600

    Milestone 1

---
+++ Assets/Scripts/Collect.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collect : MonoBehaviour {
+
+	public GameObject winText;
+
+	void OnTriggerEnter (Collider other)
+	{
+		Debug.Log ("You win");
+		Destroy (GameObject.Find("Ship Part"));
+		winText.SetActive (true);
+	}
+}
